e72c0c9c2b75dc3e55cf6e0e7cd668b93ab0cc53
saiteja37 <saitejakandadi7@gmail.com>
2022-10-22 23:58:52 +0530

    we desrvr best

---
+++ Emotion_detection/emotion_detection.py
@@ -0,0 +1,54 @@
+from keras.models import load_model
+from time import sleep
+from tensorflow.keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
+import cv2
+import numpy as np
+from playsound import playsound
+import os
+import vlc
+# for playing note.mp3 file
+
+face_classifier = cv2.CascadeClassifier(r'C:\Users\DELL\Desktop\data\Data SCience\IIIT Hyderabad\Emotion_detection\face.xml')
+classifier =load_model(r'C:\Users\DELL\Desktop\data\Data SCience\IIIT Hyderabad\Emotion_detection\model.h5')
+
+emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
+
+cap = cv2.VideoCapture(0)
+
+
+
+while True:
+    _, frame = cap.read()
+    labels = []
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+        roi_gray = gray[y:y+h,x:x+w]
+        roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+
+
+
+        if np.sum([roi_gray])!=0:
+            roi = roi_gray.astype('float')/255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi,axis=0)
+
+            prediction = classifier.predict(roi)[0]
+            label=emotion_labels[prediction.argmax()]
+            label_position = (x,y)
+            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+            if label=="Sad":
+                playsound(r'C:\Users\DELL\Desktop\data\Data SCience\IIIT Hyderabad\Emotion_detection\Nane.mp3')
+
+
+        else:
+            cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+    cv2.imshow('Emotion Detector',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
